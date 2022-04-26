ffc82310cdf4c9ce0b0798befa25d435a882ae9e
David Johnston <djohnston@cydarm.com>
2022-04-26 16:35:11 +1000

    Update example

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,84 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+// it doesn't work with either 
+import Form from "@rjsf/core"; 
+//import { MuiForm5 as Form } from '@rjsf/material-ui';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Form
+        uiSchema={{
+          "ui:submitButtonOptions": {
+            "submitText": "Confirm Details",
+            "norender": false,
+            "props": {
+              "disabled": false,
+              "className": "btn btn-info"
+            }
+          },
+          "firstName": {
+            "ui:autofocus": true,
+            "ui:emptyValue": "",
+            "ui:autocomplete": "family-name"
+          },
+          "lastName": {
+            "ui:title": "Surname",
+            "ui:emptyValue": "",
+            "ui:autocomplete": "given-name"
+          },
+          "age": {
+            "ui:widget": "updown",
+            "ui:title": "Age of person",
+            "ui:description": "(earthian year)"
+          },
+          "bio": {
+            "ui:widget": "textarea"
+          },
+          "password": {
+            "ui:widget": "password",
+            "ui:help": "Hint: Make it strong!"
+          },
+          "date": {
+            "ui:widget": "alt-datetime"
+          },
+          "telephone": {
+            "ui:options": {
+              "inputType": "tel"
+            }
+          }
+        }}
+        schema={{
+          "title": "A registration form",
+          "description": "A simple form example. Demonstrating ui options",
+          "type": "object",
+          "required": [
+            "firstName",
+            "lastName"
+          ],
+          "properties": {
+            "firstName": {
+              "type": "string",
+              "title": "First name",
+              "default": "Chuck"
+            },
+            "lastName": {
+              "type": "string",
+              "title": "Last name"
+            },
+            "telephone": {
+              "type": "string",
+              "title": "Telephone",
+              "minLength": 10
+            }
+          }
+        }}
+      />
+
+
     </div>
   );
 }
